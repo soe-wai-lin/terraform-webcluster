2299017c7d5e8c6dd4e132c84604b64bbe2d43b9
soe-wai-lin <09soewailynn@gmail.com>
2024-06-13 13:41:33 +0700

    comment out db_port and db_address

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -4,8 +4,8 @@ resource "aws_launch_configuration" "example" {
   security_groups = [aws_security_group.instance.id]
   user_data = templatefile("${path.module}/user-data.sh",{
     server_port = var.server_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
+#    db_address = data.terraform_remote_state.db.outputs.address
+#    db_port = data.terraform_remote_state.db.outputs.port
   })
 
   lifecycle {
