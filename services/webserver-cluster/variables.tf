e29a828ff2ad2abe2fda37184f2a4c2be1732085
soe-wai-lin <09soewailynn@gmail.com>
2024-06-22 16:35:18 +0700

    modify main.tf.10

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -1,6 +1,5 @@
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
-    default = 8080  
 }
 variable "cluster_name" {
   description = "The name to use for all the cluster resources"
